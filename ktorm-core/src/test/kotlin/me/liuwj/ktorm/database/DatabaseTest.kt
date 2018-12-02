60bd77abf820143ec3d928fd882ed95685b09d92
vince <me@liuwj.me>
2018-12-02 13:28:57 +0800

    test travis fail

---
+++ ktorm-core/src/test/kotlin/me/liuwj/ktorm/database/DatabaseTest.kt
@@ -0,0 +1,14 @@
+package me.liuwj.ktorm.database
+
+import org.junit.Test
+
+/**
+ * Created by vince on Dec 02, 2018.
+ */
+class DatabaseTest {
+
+    @Test
+    fun testTravis() {
+        assert(false)
+    }
+}
