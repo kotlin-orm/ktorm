847a8c72a465cbf6f2616140d26d04b1134c67c1
vince <me@liuwj.me>
2018-12-09 19:20:32 +0800

    entity aggregation tests

---
+++ ktorm-core/src/test/kotlin/me/liuwj/ktorm/entity/EntityAggregationTest.kt
@@ -0,0 +1,65 @@
+package me.liuwj.ktorm.entity
+
+import me.liuwj.ktorm.BaseTest
+import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.dsl.greater
+import me.liuwj.ktorm.dsl.minus
+import me.liuwj.ktorm.dsl.plus
+import org.junit.Test
+
+/**
+ * Created by vince on Dec 09, 2018.
+ */
+class EntityAggregationTest : BaseTest() {
+
+    @Test
+    fun testCount() {
+        val count = Employees.count { it.departmentId eq 1 }
+        assert(count == 2)
+    }
+
+    @Test
+    fun testCountAll() {
+        val count = Employees.count()
+        assert(count == 4)
+    }
+
+    @Test
+    fun testSum() {
+        val sum = Employees.sumBy { it.salary + 1 }
+        assert(sum == 454L)
+    }
+
+    @Test
+    fun testMax() {
+        val max = Employees.maxBy { it.salary - 1 }
+        assert(max == 199L)
+    }
+
+    @Test
+    fun testMin() {
+        val min = Employees.minBy { it.salary }
+        assert(min == 50L)
+    }
+
+    @Test
+    fun testAvg() {
+        val avg = Employees.avgBy { it.salary }
+        println(avg)
+    }
+
+    @Test
+    fun testNone() {
+        assert(Employees.none { it.salary greater 200L })
+    }
+
+    @Test
+    fun testAny() {
+        assert(!Employees.any { it.salary greater 200L })
+    }
+
+    @Test
+    fun testAll() {
+        assert(Employees.all { it.salary greater 0L })
+    }
+}
