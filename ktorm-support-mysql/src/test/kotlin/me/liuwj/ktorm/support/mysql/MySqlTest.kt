71e0763c8f1c5e2c64909690846f51b366ab8b10
vince <me@liuwj.me>
2018-12-13 19:38:10 +0800

    mysql tests

--- ktorm-support-mysql/src/test/kotlin/me/liuwj/ktorm/support/mysql/MySqlTest.kt
+++ ktorm-support-mysql/src/test/kotlin/me/liuwj/ktorm/support/mysql/MySqlTest.kt
@@ -4,8 +4,11 @@ import me.liuwj.ktorm.BaseTest
 import me.liuwj.ktorm.database.Database
 import me.liuwj.ktorm.database.useConnection
 import me.liuwj.ktorm.dsl.*
+import me.liuwj.ktorm.entity.count
+import me.liuwj.ktorm.entity.findById
 import me.liuwj.ktorm.entity.sumBy
 import org.junit.Test
+import java.time.LocalDate
 
 /**
  * Created by vince on Dec 12, 2018.
@@ -46,5 +49,60 @@ class MySqlTest : BaseTest() {
         }
 
         assert(Employees.sumBy { it.salary } == 800L)
+    }
+
+    @Test
+    fun testBulkInsert() {
+        Employees.bulkInsert {
+            item {
+                it.name to "jerry"
+                it.job to "trainee"
+                it.managerId to 1
+                it.hireDate to LocalDate.now()
+                it.salary to 50
+                it.departmentId to 1
+            }
+            item {
+                it.name to "linda"
+                it.job to "assistant"
+                it.managerId to 3
+                it.hireDate to LocalDate.now()
+                it.salary to 100
+                it.departmentId to 2
+            }
+        }
+
+        assert(Employees.count() == 6)
+    }
+
+    @Test
+    fun testInsertOrUpdate() {
+        Employees.insertOrUpdate {
+            it.id to 1
+            it.name to "vince"
+            it.job to "engineer"
+            it.salary to 1000
+            it.hireDate to LocalDate.now()
+            it.departmentId to 1
+
+            onDuplicateKey {
+                it.salary to it.salary + 900
+            }
+        }
+        Employees.insertOrUpdate {
+            it.id to 5
+            it.name to "vince"
+            it.job to "engineer"
+            it.salary to 1000
+            it.hireDate to LocalDate.now()
+            it.departmentId to 1
+
+            onDuplicateKey {
+                it.salary to it.salary + 900
+            }
+        }
+
+        assert(Employees.findById(1)!!.salary == 1000L)
+        assert(Employees.findById(5)!!.salary == 1000L)
     }
 }
