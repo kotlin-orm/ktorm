c3a2c82d00dcc4df6d13eaf2df770a8223a51383
vince <me@liuwj.me>
2018-12-14 15:43:20 +0800

    run upload with stacktrace

--- auto-upload.sh
+++ auto-upload.sh
@@ -4,7 +4,7 @@ last_commit=$(git log --pretty=format:'%d' | grep HEAD)
 if [[ ${last_commit} =~ "tag: " ]]
 then 
     echo "New version found, auto uploading archives to bintray..."
-    ./gradlew bintrayUpload
+    ./gradlew bintrayUpload --stacktrace
 else 
     echo "New version not found, exiting..."
 fi
