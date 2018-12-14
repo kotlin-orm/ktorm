55ad3ea08e1b318b0de18f8da83e109baaec503d
vince <me@liuwj.me>
2018-12-14 14:43:21 +0800

    brackets

--- auto-upload.sh
+++ auto-upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 last_commit=$(git log --pretty=format:'%d' | grep origin/master)
 
-if [[ $last_commit =~ "tag: " ]]
+if [[ ${last_commit} =~ "tag: " ]]
 then 
     echo "New version found, auto uploading archives to bintray..."
     ./gradlew bintrayUpload
