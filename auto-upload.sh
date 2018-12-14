2c623e5683980d172e61c6c83d02ad468d3b4651
vince <me@liuwj.me>
2018-12-14 14:42:47 +0800

    fix indent

--- auto-upload.sh
+++ auto-upload.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 last_commit=$(git log --pretty=format:'%d' | grep origin/master)
 
 if [[ $last_commit =~ "tag: " ]]
 then 
     echo "New version found, auto uploading archives to bintray..."
-	./gradlew bintrayUpload
+    ./gradlew bintrayUpload
 else 
     echo "New version not found, exiting..."
 fi
