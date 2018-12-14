646bbd5c0309af4ccd9186295e8fa3b5712aef09
vince <me@liuwj.me>
2018-12-14 14:36:55 +0800

    travis auto upload

---
+++ auto-upload.sh
@@ -0,0 +1,9 @@
+last_commit=$(git log --pretty=format:'%d' | grep origin/master)
+
+if [[ $last_commit =~ "tag: " ]]
+then 
+    echo "New version found, auto uploading archives to bintray..."
+	./gradlew bintrayUpload
+else 
+    echo "New version not found, exiting..."
+fi
