73e0ac6d242259b3b92872087772bfacca5b4f8a
vince <me@liuwj.me>
2019-04-03 22:06:34 +0800

    refactor grouping nullable key

--- ktorm-core/src/main/kotlin/me/liuwj/ktorm/entity/EntityGrouping.kt
+++ ktorm-core/src/main/kotlin/me/liuwj/ktorm/entity/EntityGrouping.kt
@@ -13,7 +13,7 @@ data class EntityGrouping<E : Entity<E>, T : Table<E>, K : Any>(
     val sequence: EntitySequence<E, T>,
     val keySelector: (T) -> ColumnDeclaring<K>
 ) {
-    fun asKotlinGrouping() = object : Grouping<E, K> {
+    fun asKotlinGrouping() = object : Grouping<E, K?> {
         private val allEntities = LinkedHashMap<E, K?>()
 
         init {
@@ -31,36 +31,36 @@ data class EntityGrouping<E : Entity<E>, T : Table<E>, K : Any>(
             return allEntities.keys.iterator()
         }
 
-        override fun keyOf(element: E): K {
-            return allEntities[element] ?: error("The grouping key is null for element $element")
+        override fun keyOf(element: E): K? {
+            return allEntities[element]
         }
     }
 }
 
 inline fun <E : Entity<E>, K : Any, R> EntityGrouping<E, *, K>.aggregate(
-    operation: (key: K, accumulator: R?, element: E, first: Boolean) -> R
-): Map<K, R> {
+    operation: (key: K?, accumulator: R?, element: E, first: Boolean) -> R
+): Map<K?, R> {
     return asKotlinGrouping().aggregate(operation)
 }
 
-inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K, R>> EntityGrouping<E, *, K>.aggregateTo(
+inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K?, R>> EntityGrouping<E, *, K>.aggregateTo(
     destination: M,
-    operation: (key: K, accumulator: R?, element: E, first: Boolean) -> R
+    operation: (key: K?, accumulator: R?, element: E, first: Boolean) -> R
 ): M {
     return asKotlinGrouping().aggregateTo(destination, operation)
 }
 
 inline fun <E : Entity<E>, K : Any, R> EntityGrouping<E, *, K>.fold(
-    initialValueSelector: (key: K, element: E) -> R,
-    operation: (key: K, accumulator: R, element: E) -> R
-): Map<K, R> {
+    initialValueSelector: (key: K?, element: E) -> R,
+    operation: (key: K?, accumulator: R, element: E) -> R
+): Map<K?, R> {
     return asKotlinGrouping().fold(initialValueSelector, operation)
 }
 
-inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K, R>> EntityGrouping<E, *, K>.foldTo(
+inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K?, R>> EntityGrouping<E, *, K>.foldTo(
     destination: M,
-    initialValueSelector: (key: K, element: E) -> R,
-    operation: (key: K, accumulator: R, element: E) -> R
+    initialValueSelector: (key: K?, element: E) -> R,
+    operation: (key: K?, accumulator: R, element: E) -> R
 ): M {
     return asKotlinGrouping().foldTo(destination, initialValueSelector, operation)
 }
@@ -68,11 +68,11 @@ inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K, R>> EntityGrouping<E
 inline fun <E : Entity<E>, K : Any, R> EntityGrouping<E, *, K>.fold(
     initialValue: R,
     operation: (accumulator: R, element: E) -> R
-): Map<K, R> {
+): Map<K?, R> {
     return asKotlinGrouping().fold(initialValue, operation)
 }
 
-inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K, R>> EntityGrouping<E, *, K>.foldTo(
+inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K?, R>> EntityGrouping<E, *, K>.foldTo(
     destination: M,
     initialValue: R,
     operation: (accumulator: R, element: E) -> R
@@ -81,26 +81,25 @@ inline fun <E : Entity<E>, K : Any, R, M : MutableMap<in K, R>> EntityGrouping<E
 }
 
 inline fun <E : Entity<E>, K : Any> EntityGrouping<E, *, K>.reduce(
-    operation: (key: K, accumulator: E, element: E) -> E
-): Map<K, E> {
+    operation: (key: K?, accumulator: E, element: E) -> E
+): Map<K?, E> {
     return asKotlinGrouping().reduce(operation)
 }
 
-inline fun <E : Entity<E>, K : Any, M : MutableMap<in K, E>> EntityGrouping<E, *, K>.reduceTo(
+inline fun <E : Entity<E>, K : Any, M : MutableMap<in K?, E>> EntityGrouping<E, *, K>.reduceTo(
     destination: M,
-    operation: (key: K, accumulator: E, element: E) -> E
+    operation: (key: K?, accumulator: E, element: E) -> E
 ): M {
     return asKotlinGrouping().reduceTo(destination, operation)
 }
 
-fun <E : Entity<E>, T : Table<E>, K : Any> EntityGrouping<E, T, K>.eachCount(): Map<K, Int> {
+fun <E : Entity<E>, T : Table<E>, K : Any> EntityGrouping<E, T, K>.eachCount(): Map<K?, Int> {
     return eachCountTo(LinkedHashMap())
 }
 
-fun <E : Entity<E>, T : Table<E>, K : Any, M : MutableMap<in K, Int>> EntityGrouping<E, T, K>.eachCountTo(
+fun <E : Entity<E>, T : Table<E>, K : Any, M : MutableMap<in K?, Int>> EntityGrouping<E, T, K>.eachCountTo(
     destination: M
 ): M {
-    val keyColumn = keySelector(sequence.sourceTable)
     val countExpr = AggregateExpression(
         type = AggregateType.COUNT,
         argument = null,
@@ -108,40 +107,38 @@ fun <E : Entity<E>, T : Table<E>, K : Any, M : MutableMap<in K, Int>> EntityGrou
         sqlType = IntSqlType
     )
 
-    val expr = sequence.expression.copy(
-        columns = listOf(keyColumn.asExpression(), countExpr).map { ColumnDeclaringExpression(it) },
-        groupBy = listOf(keyColumn.asExpression())
-    )
-
-    for (row in Query(expr)) {
-        val groupKey = keyColumn.sqlType.getResult(row, 1) ?: error("One of the grouping key is null.")
-        destination[groupKey] = row.getInt(2)
-    }
-
-    return destination
+    @Suppress("UNCHECKED_CAST")
+    return aggregateTo(destination as MutableMap<in K?, Int?>, countExpr) as M
 }
 
 inline fun <E : Entity<E>, T : Table<E>, K : Any, C : Number> EntityGrouping<E, T, K>.eachSumOf(
     columnSelector: (T) -> ColumnDeclaring<C>
-): Map<K, C> {
+): Map<K?, C?> {
     return eachSumOfTo(LinkedHashMap(), columnSelector)
 }
 
-inline fun <E : Entity<E>, T : Table<E>, K : Any, C : Number, M : MutableMap<in K, in C>> EntityGrouping<E, T, K>.eachSumOfTo(
+inline fun <E : Entity<E>, T : Table<E>, K : Any, C : Number, M : MutableMap<in K?, in C?>> EntityGrouping<E, T, K>.eachSumOfTo(
     destination: M,
     columnSelector: (T) -> ColumnDeclaring<C>
 ): M {
-    val keyColumn = keySelector(sequence.sourceTable)
-    val aggregation = sum(columnSelector(sequence.sourceTable))
+    return aggregateTo(destination, sum(columnSelector(sequence.sourceTable)))
+}
+
+fun <E : Entity<E>, T : Table<E>, K : Any, C : Any, M : MutableMap<in K?, in C?>> EntityGrouping<E, T, K>.aggregateTo(
+    destination: M,
+    aggregation: AggregateExpression<C>
+): M {
+    val keyColumn = keySelector(sequence.sourceTable).asExpression()
 
     val expr = sequence.expression.copy(
-        columns = listOf(keyColumn.asExpression(), aggregation).map { ColumnDeclaringExpression(it) },
-        groupBy = listOf(keyColumn.asExpression())
+        columns = listOf(keyColumn, aggregation).map { ColumnDeclaringExpression(it) },
+        groupBy = listOf(keyColumn)
     )
 
     for (row in Query(expr)) {
-        val groupKey = keyColumn.sqlType.getResult(row, 1) ?: error("One of the grouping key is null.")
-        destination[groupKey] = aggregation.sqlType.getResult(row, 2) ?: error("One of the aggregation result is null")
+        val key = keyColumn.sqlType.getResult(row, 1)
+        val value = aggregation.sqlType.getResult(row, 2)
+        destination[key] = value
     }
 
     return destination
