de733f97bfbb8ac2e03c1a1e458e7fd088dd05b2
vince <me@liuwj.me>
2019-05-12 16:53:53 +0800

    documents for join functions

--- ktorm-core/src/main/kotlin/me/liuwj/ktorm/dsl/Join.kt
+++ ktorm-core/src/main/kotlin/me/liuwj/ktorm/dsl/Join.kt
@@ -1,12 +1,28 @@
-/**
- * 该文件为 Table 及 QuerySourceExpression 提供了扩展函数，以支持各种形式的联表
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package me.liuwj.ktorm.dsl
 
 import me.liuwj.ktorm.expression.*
 import me.liuwj.ktorm.schema.ColumnDeclaring
 import me.liuwj.ktorm.schema.Table
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `cross join` in SQL.
+ */
 fun QuerySourceExpression.crossJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -14,6 +30,9 @@ fun QuerySourceExpression.crossJoin(
     return JoinExpression(type = JoinType.CROSS_JOIN, left = this, right = right, condition = on?.asExpression())
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `cross join` in SQL.
+ */
 fun QuerySourceExpression.crossJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
@@ -21,6 +40,9 @@ fun QuerySourceExpression.crossJoin(
     return crossJoin(right.asExpression(), on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `cross join` in SQL.
+ */
 fun Table<*>.crossJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -28,6 +50,9 @@ fun Table<*>.crossJoin(
     return asExpression().crossJoin(right, on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `cross join` in SQL.
+ */
 fun Table<*>.crossJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
@@ -35,6 +60,9 @@ fun Table<*>.crossJoin(
     return crossJoin(right.asExpression(), on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `inner join` in SQL.
+ */
 fun QuerySourceExpression.innerJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -42,6 +70,9 @@ fun QuerySourceExpression.innerJoin(
     return JoinExpression(type = JoinType.INNER_JOIN, left = this, right = right, condition = on?.asExpression())
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `inner join` in SQL.
+ */
 fun QuerySourceExpression.innerJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
@@ -49,6 +80,9 @@ fun QuerySourceExpression.innerJoin(
     return innerJoin(right.asExpression(), on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `inner join` in SQL.
+ */
 fun Table<*>.innerJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -56,6 +90,9 @@ fun Table<*>.innerJoin(
     return asExpression().innerJoin(right, on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `inner join` in SQL.
+ */
 fun Table<*>.innerJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
@@ -63,6 +100,9 @@ fun Table<*>.innerJoin(
     return innerJoin(right.asExpression(), on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `left join` in SQL.
+ */
 fun QuerySourceExpression.leftJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -70,6 +110,9 @@ fun QuerySourceExpression.leftJoin(
     return JoinExpression(type = JoinType.LEFT_JOIN, left = this, right = right, condition = on?.asExpression())
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `left join` in SQL.
+ */
 fun QuerySourceExpression.leftJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
@@ -77,6 +120,9 @@ fun QuerySourceExpression.leftJoin(
     return leftJoin(right.asExpression(), on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `left join` in SQL.
+ */
 fun Table<*>.leftJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -84,6 +130,9 @@ fun Table<*>.leftJoin(
     return asExpression().leftJoin(right, on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `left join` in SQL.
+ */
 fun Table<*>.leftJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
@@ -91,6 +140,9 @@ fun Table<*>.leftJoin(
     return leftJoin(right.asExpression(), on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `right join` in SQL.
+ */
 fun QuerySourceExpression.rightJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -98,6 +150,9 @@ fun QuerySourceExpression.rightJoin(
     return JoinExpression(type = JoinType.RIGHT_JOIN, left = this, right = right, condition = on?.asExpression())
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `right join` in SQL.
+ */
 fun QuerySourceExpression.rightJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
@@ -105,6 +160,9 @@ fun QuerySourceExpression.rightJoin(
     return rightJoin(right.asExpression(), on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `right join` in SQL.
+ */
 fun Table<*>.rightJoin(
     right: QuerySourceExpression,
     on: ColumnDeclaring<Boolean>? = null
@@ -112,6 +170,9 @@ fun Table<*>.rightJoin(
     return asExpression().rightJoin(right, on)
 }
 
+/**
+ * Join the right table and return a [JoinExpression], equivalent to `right join` in SQL.
+ */
 fun Table<*>.rightJoin(
     right: Table<*>,
     on: ColumnDeclaring<Boolean>? = null
