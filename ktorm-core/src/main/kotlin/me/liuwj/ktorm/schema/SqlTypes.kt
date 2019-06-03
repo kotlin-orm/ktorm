e471160546eb539cbcb8dec3951975678140c6d5
vince <me@liuwj.me>
2019-06-03 11:45:15 +0800

    documents for SqlTypes

--- ktorm-core/src/main/kotlin/me/liuwj/ktorm/schema/SqlTypes.kt
+++ ktorm-core/src/main/kotlin/me/liuwj/ktorm/schema/SqlTypes.kt
@@ -1,6 +1,19 @@
-/**
- * 该文件提供 Table 类的扩展函数，以支持往表中注册各种不同类型的字段，该文件中的所有函数均直接调用 Table.registerColumn 方法
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package me.liuwj.ktorm.schema
 
 import me.liuwj.ktorm.entity.Entity
@@ -13,10 +26,16 @@ import java.time.format.DateTimeFormatterBuilder
 import java.time.format.SignStyle
 import java.time.temporal.ChronoField
 
+/**
+ * Define a column typed of [BooleanSqlType].
+ */
 fun <E : Entity<E>> Table<E>.boolean(name: String): Table<E>.ColumnRegistration<Boolean> {
     return registerColumn(name, BooleanSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `boolean` SQL type.
+ */
 object BooleanSqlType : SqlType<Boolean>(Types.BOOLEAN, "boolean") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: Boolean) {
         ps.setBoolean(index, parameter)
@@ -27,10 +46,16 @@ object BooleanSqlType : SqlType<Boolean>(Types.BOOLEAN, "boolean") {
     }
 }
 
+/**
+ * Define a column typed of [IntSqlType].
+ */
 fun <E : Entity<E>> Table<E>.int(name: String): Table<E>.ColumnRegistration<Int> {
     return registerColumn(name, IntSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `int` SQL type.
+ */
 object IntSqlType : SqlType<Int>(Types.INTEGER, "int") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: Int) {
         ps.setInt(index, parameter)
@@ -41,10 +66,16 @@ object IntSqlType : SqlType<Int>(Types.INTEGER, "int") {
     }
 }
 
+/**
+ * Define a column typed of [LongSqlType].
+ */
 fun <E : Entity<E>> Table<E>.long(name: String): Table<E>.ColumnRegistration<Long> {
     return registerColumn(name, LongSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `long` SQL type.
+ */
 object LongSqlType : SqlType<Long>(Types.BIGINT, "bigint") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: Long) {
         ps.setLong(index, parameter)
@@ -55,10 +86,16 @@ object LongSqlType : SqlType<Long>(Types.BIGINT, "bigint") {
     }
 }
 
+/**
+ * Define a column typed of [FloatSqlType].
+ */
 fun <E : Entity<E>> Table<E>.float(name: String): Table<E>.ColumnRegistration<Float> {
     return registerColumn(name, FloatSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `float` SQL type.
+ */
 object FloatSqlType : SqlType<Float>(Types.FLOAT, "float") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: Float) {
         ps.setFloat(index, parameter)
@@ -69,10 +106,16 @@ object FloatSqlType : SqlType<Float>(Types.FLOAT, "float") {
     }
 }
 
+/**
+ * Define a column typed of [DoubleSqlType].
+ */
 fun <E : Entity<E>> Table<E>.double(name: String): Table<E>.ColumnRegistration<Double> {
     return registerColumn(name, DoubleSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `double` SQL type.
+ */
 object DoubleSqlType : SqlType<Double>(Types.DOUBLE, "double") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: Double) {
         ps.setDouble(index, parameter)
@@ -83,10 +126,16 @@ object DoubleSqlType : SqlType<Double>(Types.DOUBLE, "double") {
     }
 }
 
+/**
+ * Define a column typed of [DecimalSqlType].
+ */
 fun <E : Entity<E>> Table<E>.decimal(name: String): Table<E>.ColumnRegistration<BigDecimal> {
     return registerColumn(name, DecimalSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `decimal` SQL type.
+ */
 object DecimalSqlType : SqlType<BigDecimal>(Types.DECIMAL, "decimal") {
 
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: BigDecimal) {
@@ -98,10 +147,16 @@ object DecimalSqlType : SqlType<BigDecimal>(Types.DECIMAL, "decimal") {
     }
 }
 
+/**
+ * Define a column typed of [VarcharSqlType].
+ */
 fun <E : Entity<E>> Table<E>.varchar(name: String): Table<E>.ColumnRegistration<String> {
     return registerColumn(name, VarcharSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `varchar` SQL type.
+ */
 object VarcharSqlType : SqlType<String>(Types.VARCHAR, "varchar") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: String) {
         ps.setString(index, parameter)
@@ -112,10 +167,16 @@ object VarcharSqlType : SqlType<String>(Types.VARCHAR, "varchar") {
     }
 }
 
+/**
+ * Define a column typed of [TextSqlType].
+ */
 fun <E : Entity<E>> Table<E>.text(name: String): Table<E>.ColumnRegistration<String> {
     return registerColumn(name, TextSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `text` SQL type.
+ */
 object TextSqlType : SqlType<String>(Types.LONGVARCHAR, "text") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: String) {
         ps.setString(index, parameter)
@@ -126,10 +187,16 @@ object TextSqlType : SqlType<String>(Types.LONGVARCHAR, "text") {
     }
 }
 
+/**
+ * Define a column typed of [BlobSqlType].
+ */
 fun <E : Entity<E>> Table<E>.blob(name: String): Table<E>.ColumnRegistration<ByteArray> {
     return registerColumn(name, BlobSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `blob` SQL type.
+ */
 object BlobSqlType : SqlType<ByteArray>(Types.BLOB, "blob") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: ByteArray) {
         ByteArrayInputStream(parameter).use { ps.setBlob(index, it) }
@@ -140,10 +207,16 @@ object BlobSqlType : SqlType<ByteArray>(Types.BLOB, "blob") {
     }
 }
 
+/**
+ * Define a column typed of [LocalDateTimeSqlType].
+ */
 fun <E : Entity<E>> Table<E>.datetime(name: String): Table<E>.ColumnRegistration<LocalDateTime> {
     return registerColumn(name, LocalDateTimeSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `datetime` SQL type.
+ */
 object LocalDateTimeSqlType : SqlType<LocalDateTime>(Types.TIMESTAMP, "datetime") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: LocalDateTime) {
         ps.setTimestamp(index, Timestamp.valueOf(parameter))
@@ -154,10 +227,16 @@ object LocalDateTimeSqlType : SqlType<LocalDateTime>(Types.TIMESTAMP, "datetime"
     }
 }
 
+/**
+ * Define a column typed of [LocalDateSqlType].
+ */
 fun <E : Entity<E>> Table<E>.date(name: String): Table<E>.ColumnRegistration<LocalDate> {
     return registerColumn(name, LocalDateSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `date` SQL type.
+ */
 object LocalDateSqlType : SqlType<LocalDate>(Types.DATE, "date") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: LocalDate) {
         ps.setDate(index, Date.valueOf(parameter))
@@ -168,10 +247,16 @@ object LocalDateSqlType : SqlType<LocalDate>(Types.DATE, "date") {
     }
 }
 
+/**
+ * Define a column typed of [LocalTimeSqlType].
+ */
 fun <E : Entity<E>> Table<E>.time(name: String): Table<E>.ColumnRegistration<LocalTime> {
     return registerColumn(name, LocalTimeSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `time` SQL type.
+ */
 object LocalTimeSqlType : SqlType<LocalTime>(Types.TIME, "time") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: LocalTime) {
         ps.setTime(index, Time.valueOf(parameter))
@@ -182,10 +267,16 @@ object LocalTimeSqlType : SqlType<LocalTime>(Types.TIME, "time") {
     }
 }
 
+/**
+ * Define a column typed of [MonthDaySqlType], instances of [MonthDay] are saved as strings in format `MM-dd`.
+ */
 fun <E : Entity<E>> Table<E>.monthDay(name: String): Table<E>.ColumnRegistration<MonthDay> {
     return registerColumn(name, MonthDaySqlType)
 }
 
+/**
+ * [SqlType] implementation used to save [MonthDay] instances, formating them to strings with pattern `MM-dd`.
+ */
 object MonthDaySqlType : SqlType<MonthDay>(Types.VARCHAR, "varchar") {
     val formatter: DateTimeFormatter = DateTimeFormatterBuilder()
         .appendValue(ChronoField.MONTH_OF_YEAR, 2)
@@ -202,10 +293,16 @@ object MonthDaySqlType : SqlType<MonthDay>(Types.VARCHAR, "varchar") {
     }
 }
 
+/**
+ * Define a column typed of [YearMonthSqlType], instances of [YearMonth] are saved as strings in format `yyyy-MM`.
+ */
 fun <E : Entity<E>> Table<E>.yearMonth(name: String): Table<E>.ColumnRegistration<YearMonth> {
     return registerColumn(name, YearMonthSqlType)
 }
 
+/**
+ * [SqlType] implementation used to save [YearMonth] instances, formating them to strings with pattern `yyyy-MM`.
+ */
 @Suppress("MagicNumber")
 object YearMonthSqlType : SqlType<YearMonth>(Types.VARCHAR, "varchar") {
     val formatter: DateTimeFormatter = DateTimeFormatterBuilder()
@@ -223,10 +320,16 @@ object YearMonthSqlType : SqlType<YearMonth>(Types.VARCHAR, "varchar") {
     }
 }
 
+/**
+ * Define a column typed of [YearSqlType], instances of [Year] are saved as integers.
+ */
 fun <E : Entity<E>> Table<E>.year(name: String): Table<E>.ColumnRegistration<Year> {
     return registerColumn(name, YearSqlType)
 }
 
+/**
+ * [SqlType] implementation used to save [Year] instances as integers.
+ */
 object YearSqlType : SqlType<Year>(Types.INTEGER, "int") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: Year) {
         ps.setInt(index, parameter.value)
@@ -237,10 +340,16 @@ object YearSqlType : SqlType<Year>(Types.INTEGER, "int") {
     }
 }
 
+/**
+ * Define a column of typed [InstantSqlType].
+ */
 fun <E : Entity<E>> Table<E>.timestamp(name: String): Table<E>.ColumnRegistration<Instant> {
     return registerColumn(name, InstantSqlType)
 }
 
+/**
+ * [SqlType] implementation represents `timestamp` SQL type.
+ */
 object InstantSqlType : SqlType<Instant>(Types.TIMESTAMP, "timestamp") {
     override fun doSetParameter(ps: PreparedStatement, index: Int, parameter: Instant) {
         ps.setTimestamp(index, Timestamp.from(parameter))
