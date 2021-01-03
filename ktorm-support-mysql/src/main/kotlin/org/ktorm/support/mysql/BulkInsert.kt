8df51a993ad8fa7e3cf80a55dab9a9233d898bc6
vince <me@liuwj.me>
2021-01-04 00:46:41 +0800

    update comment

--- ktorm-support-mysql/src/main/kotlin/org/ktorm/support/mysql/BulkInsert.kt
+++ ktorm-support-mysql/src/main/kotlin/org/ktorm/support/mysql/BulkInsert.kt
@@ -134,7 +134,7 @@ public fun <T : BaseTable<*>> Database.bulkInsert(
  * ```sql
  * insert into t_employee (id, name, job, salary, hire_date, department_id)
  * values (?, ?, ?, ?, ?, ?), (?, ?, ?, ?, ?, ?)
- * on duplicate key update salary = t_employee.salary + ?
+ * on duplicate key update salary = salary + ?
  * ```
  *
  * @since 3.3.0
