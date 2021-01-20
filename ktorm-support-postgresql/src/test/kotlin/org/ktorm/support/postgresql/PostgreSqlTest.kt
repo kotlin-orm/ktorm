4a9bb7cd1d48bb254bbfc03f6cbe6487a4eef73d
Sigmund Marius Nilssen <sigmund.nilssen@byr.oslo.kommune.no>
2021-01-20 10:05:48 +0100

    Removing sys.err.printlns

--- ktorm-support-postgresql/src/test/kotlin/org/ktorm/support/postgresql/PostgreSqlTest.kt
+++ ktorm-support-postgresql/src/test/kotlin/org/ktorm/support/postgresql/PostgreSqlTest.kt
@@ -501,8 +501,6 @@ class PostgreSqlTest : BaseTest() {
         val skippedFuture = Executors.newSingleThreadExecutor().submit {
             assertTrue(readyToTestSkipLocksLatch.await(5, TimeUnit.SECONDS))
             val employees = database.useTransaction(isolation = TransactionIsolation.REPEATABLE_READ) {
-                System.err.println("Cake: ${database.transactionManager.currentTransaction}")
-
                 database
                     .sequenceOf(Employees, withReferences = false)
                     .filter { it.id eq 1 }
@@ -512,14 +510,12 @@ class PostgreSqlTest : BaseTest() {
             }
             testSkipLocksDoneLatch.countDown()
             if (employees.isNotEmpty()) {
-                System.err.println(employees)
                 throw java.lang.IllegalStateException("Entry should have been skipped due to being locked.")
             }
         }
 
         val selectedFuture = Executors.newSingleThreadExecutor().submit {
             val employees = database.useTransaction(isolation = TransactionIsolation.REPEATABLE_READ) {
-                System.err.println(database.transactionManager.currentTransaction)
                 val employeeList = database
                     .sequenceOf(Employees, withReferences = false)
                     .filter { it.id eq 1 }
