2de50b2b8ea12cd02fcbfd2025451ccac8147487
vince <me@liuwj.me>
2021-05-17 00:01:46 +0800

    refactor publish script

--- ktorm-support-sqlserver/ktorm-support-sqlserver.gradle.kts
+++ ktorm-support-sqlserver/ktorm-support-sqlserver.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     api(project(":ktorm-core"))
-    compileOnly("com.microsoft.sqlserver:mssql-jdbc:7.2.2.jre8")
+    api("com.microsoft.sqlserver:mssql-jdbc:7.2.2.jre8")
     testImplementation(project(":ktorm-core", configuration = "testOutput"))
     testImplementation("org.testcontainers:mssqlserver:1.15.1")
 }
