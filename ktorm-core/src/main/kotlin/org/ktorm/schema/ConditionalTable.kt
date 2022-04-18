5fcf33db50f1d6b249ea2bf4c52e6adbe3f50814
ForteScarlet <ForteScarlet@163.com>
2022-04-18 22:44:09 +0800

    :bulb: 添加源码注释

--- ktorm-core/src/main/kotlin/org/ktorm/schema/ConditionalTable.kt
+++ ktorm-core/src/main/kotlin/org/ktorm/schema/ConditionalTable.kt
@@ -20,7 +20,7 @@ import kotlin.reflect.KClass
  * }
  *
  *
- * object Departments : Table<Department>("t_department") {
+ * object Departments : ConditionalTable<Department>("t_department") {
  *    val id = int("id").primaryKey().bindTo { it.id }.conditionOn { department, column, value ->
  *      if (value != null) column eq value else column eq 1
  *    }
