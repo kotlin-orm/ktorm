d3d52897f0539508d415e8c00a6495e6c1b52ed5
vince <me@liuwj.me>
2022-09-21 20:50:12 +0800

    fix detekt problems

--- ktorm-support-mysql/src/main/kotlin/org/ktorm/support/mysql/DefaultValue.kt
+++ ktorm-support-mysql/src/main/kotlin/org/ktorm/support/mysql/DefaultValue.kt
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+@file:Suppress("MatchingDeclarationName")
+
 package org.ktorm.support.mysql
 
 import org.ktorm.expression.ScalarExpression
--- ktorm-support-postgresql/src/main/kotlin/org/ktorm/support/postgresql/DefaultValue.kt
+++ ktorm-support-postgresql/src/main/kotlin/org/ktorm/support/postgresql/DefaultValue.kt
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+@file:Suppress("MatchingDeclarationName")
+
 package org.ktorm.support.postgresql
 
 import org.ktorm.expression.ScalarExpression
