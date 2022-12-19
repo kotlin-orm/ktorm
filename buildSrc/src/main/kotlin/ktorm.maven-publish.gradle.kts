5aeff5537bdf7a32bddc252601f4306dcbff08a7
robalmeister <65306693+robalmeister@users.noreply.github.com>
2022-12-19 18:28:56 +0100

    Update ktorm.maven-publish.gradle.kts

--- buildSrc/src/main/kotlin/ktorm.maven-publish.gradle.kts
+++ buildSrc/src/main/kotlin/ktorm.maven-publish.gradle.kts
@@ -131,6 +131,11 @@ publishing {
                         name.set("ccr")
                         email.set("2938137849@qq.com")
                     }
+                    developer {
+                        id.set("robalmeister")
+                        name.set("Filip Wlodarczyk")
+                        email.set("robalmeister@shinemc.pl")
+                    }
                 }
             }
         }
