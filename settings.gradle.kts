00c2bfbb637090f4d226507a65d1fbd8c1a11c02
vince <me@liuwj.me>
2022-12-19 21:36:40 +0800

    enable gradle build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,8 @@
 
+plugins {
+    id("com.gradle.enterprise") version("3.9")
+}
+
 include("ktorm-core")
 include("ktorm-global")
 include("ktorm-jackson")
@@ -11,4 +15,14 @@ include("ktorm-support-sqlserver")
 rootProject.name = "ktorm"
 rootProject.children.forEach { project ->
     project.buildFileName = "${project.name}.gradle.kts"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") == "true") {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
 }
