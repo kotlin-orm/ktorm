e007f543be04dd3c439cab703f674ca860b75111
vince <me@liuwj.me>
2023-01-14 15:55:26 +0800

    test sum & count

--- ktorm-core/src/test/kotlin/org/ktorm/dsl/WindowFunctionTest.kt
+++ ktorm-core/src/test/kotlin/org/ktorm/dsl/WindowFunctionTest.kt
@@ -330,4 +330,52 @@ class WindowFunctionTest : BaseTest() {
 
         assertEquals(setOf("vince:100.00", "marry:75.00", "tom:117.00", "penny:113.00"), results.toSet())
     }
+
+    @Test
+    fun testSum() {
+        val results = database
+            .from(Employees)
+            .select(Employees.name, sum(Employees.salary).over())
+            .map { row ->
+                "${row.getString(1)}:${row.getLong(2)}"
+            }
+
+        assertEquals(setOf("vince:450", "marry:450", "tom:450", "penny:450"), results.toSet())
+    }
+
+    @Test
+    fun testRunningSum() {
+        val results = database
+            .from(Employees)
+            .select(Employees.name, sum(Employees.salary).over { orderBy(Employees.id.asc()) })
+            .map { row ->
+                "${row.getString(1)}:${row.getLong(2)}"
+            }
+
+        assertEquals(setOf("vince:100", "marry:150", "tom:350", "penny:450"), results.toSet())
+    }
+
+    @Test
+    fun testCount() {
+        val results = database
+            .from(Employees)
+            .select(Employees.name, count(Employees.salary).over())
+            .map { row ->
+                "${row.getString(1)}:${row.getInt(2)}"
+            }
+
+        assertEquals(setOf("vince:4", "marry:4", "tom:4", "penny:4"), results.toSet())
+    }
+
+    @Test
+    fun testRunningCount() {
+        val results = database
+            .from(Employees)
+            .select(Employees.name, count(Employees.salary).over { orderBy(Employees.id.asc()) })
+            .map { row ->
+                "${row.getString(1)}:${row.getInt(2)}"
+            }
+
+        assertEquals(setOf("vince:1", "marry:2", "tom:3", "penny:4"), results.toSet())
+    }
 }
