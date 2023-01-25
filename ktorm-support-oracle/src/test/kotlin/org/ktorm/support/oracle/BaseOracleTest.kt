f8386fec4978cba2c5d888e5104a0130ba9a1df3
vince <me@liuwj.me>
2023-01-25 22:30:51 +0800

    try fix oracle connection issue

--- ktorm-support-oracle/src/test/kotlin/org/ktorm/support/oracle/BaseOracleTest.kt
+++ ktorm-support-oracle/src/test/kotlin/org/ktorm/support/oracle/BaseOracleTest.kt
@@ -8,8 +8,12 @@ import java.sql.DriverManager
 import kotlin.concurrent.thread
 
 abstract class BaseOracleTest : BaseTest() {
+    lateinit var connection: Connection
 
     override fun init() {
+        Class.forName(driverClassName)
+        connection = DriverManager.getConnection(jdbcUrl, username, password)
+
         database = Database.connect(alwaysQuoteIdentifiers = true) {
             object : Connection by connection {
                 override fun close() {
@@ -23,11 +27,10 @@ abstract class BaseOracleTest : BaseTest() {
 
     override fun destroy() {
         execSqlScript("drop-oracle-data.sql")
+        connection.close()
     }
 
     companion object : OracleContainer("zerda/oracle-database:11.2.0.2-xe") {
-        lateinit var connection: Connection
-
         init {
             // At least 1 GB memory is required by Oracle.
             withCreateContainerCmdModifier { cmd -> cmd.hostConfig?.withShmSize((1 * 1024 * 1024 * 1024).toLong()) }
@@ -35,18 +38,6 @@ abstract class BaseOracleTest : BaseTest() {
             start()
             // Stop the container when the process exits.
             Runtime.getRuntime().addShutdownHook(thread(start = false) { stop() })
-        }
-
-        override fun start() {
-            super.start()
-
-            Class.forName(driverClassName)
-            connection = DriverManager.getConnection(jdbcUrl, username, password)
-        }
-
-        override fun stop() {
-            connection.close()
-            super.stop()
         }
     }
 }
