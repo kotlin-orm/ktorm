ef8838ef28400987b4541981a03f61d621d40921
vince <me@liuwj.me>
2023-08-13 18:09:36 +0800

    use ksp cmdline

--- ktorm-ksp-compiler-maven-plugin/ktorm-ksp-compiler-maven-plugin.gradle.kts
+++ ktorm-ksp-compiler-maven-plugin/ktorm-ksp-compiler-maven-plugin.gradle.kts
@@ -8,5 +8,5 @@ plugins {
 dependencies {
     compileOnly(kotlin("maven-plugin"))
     compileOnly("org.apache.maven:maven-core:3.9.3")
-    implementation("com.google.devtools.ksp:symbol-processing:1.7.22-1.0.8")
+    implementation("com.google.devtools.ksp:symbol-processing-cmdline:1.7.22-1.0.8")
 }
