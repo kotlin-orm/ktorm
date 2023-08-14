ea6b7b2804ae6154156fbac87dc98bcc72199e70
vince <me@liuwj.me>
2023-08-14 23:06:25 +0800

    parse user optionns

--- ktorm-ksp-compiler-maven-plugin/src/main/kotlin/org/ktorm/ksp/compiler/maven/KtormKspMavenPluginExtension.kt
+++ ktorm-ksp-compiler-maven-plugin/src/main/kotlin/org/ktorm/ksp/compiler/maven/KtormKspMavenPluginExtension.kt
@@ -1,8 +1,7 @@
 package org.ktorm.ksp.compiler.maven
 
-import org.apache.maven.artifact.resolver.ArtifactResolutionRequest
+import com.google.devtools.ksp.KspCliOption
 import org.apache.maven.plugin.MojoExecution
-import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.project.MavenProject
 import org.apache.maven.repository.RepositorySystem
 import org.codehaus.plexus.component.annotations.Component
@@ -27,11 +26,24 @@ public class KtormKspMavenPluginExtension : KotlinMavenPluginExtension {
     }
 
     override fun getPluginOptions(project: MavenProject, execution: MojoExecution): List<PluginOption> {
-        val request = ArtifactResolutionRequest()
-        request.artifact = repositorySystem.createArtifactWithClassifier("com.pinterest", "ktlint", "0.50.0", "jar", "all")
-        request.remoteRepositories = project.remoteArtifactRepositories
+//        val request = ArtifactResolutionRequest()
+//        request.artifact = repositorySystem.createArtifactWithClassifier("com.pinterest", "ktlint", "0.50.0", "jar", "all")
+//        request.remoteRepositories = project.remoteArtifactRepositories
+//
+//        val resolved = repositorySystem.resolve(request)
+//        throw MojoExecutionException("test ktorm ksp: ${resolved.artifacts.map { it.file }}")
+        return emptyList()
+    }
 
-        val resolved = repositorySystem.resolve(request)
-        throw MojoExecutionException("test ktorm ksp: ${resolved.artifacts.map { it.file }}")
+    private fun parseUserOptions(execution: MojoExecution): Map<KspCliOption, List<String>> {
+        val pluginOptions = execution.configuration.getChild("pluginOptions") ?: return emptyMap()
+        val availableOptions = KspCliOption.values().associateBy { it.optionName }
+        val pattern = Regex("([^:]+):([^=]+)=(.*)")
+
+        return pluginOptions.children
+            .mapNotNull { pattern.matchEntire(it.value) }
+            .map { it.destructured }
+            .filter { (plugin, key, _) -> plugin == "ksp" && key in availableOptions }
+            .groupBy({ (_, key, _) -> availableOptions[key]!! }, { (_, _, value) -> value })
     }
 }
