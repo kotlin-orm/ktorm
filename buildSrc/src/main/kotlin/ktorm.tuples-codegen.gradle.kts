61ccfd27f500022150407b882012fe6a07059b71
vince <me@liuwj.me>
2024-04-20 18:37:41 +0800

    update copyright year

--- buildSrc/src/main/kotlin/ktorm.tuples-codegen.gradle.kts
+++ buildSrc/src/main/kotlin/ktorm.tuples-codegen.gradle.kts
@@ -282,7 +282,7 @@ val generateTuples by tasks.registering {
         outputFile.bufferedWriter().use { writer ->
             writer.write("""
                 /*
-                 * Copyright 2018-2023 the original author or authors.
+                 * Copyright 2018-2024 the original author or authors.
                  *
                  * Licensed under the Apache License, Version 2.0 (the "License");
                  * you may not use this file except in compliance with the License.
