a7edb4aacd5fd2bc7dba8575e0d8fade05972764
vince <me@liuwj.me>
2024-07-15 17:53:14 +0800

    upgrade detekt version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,5 +11,5 @@ repositories {
 dependencies {
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     api("org.moditect:moditect-gradle-plugin:1.0.0-rc3")
-    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.1")
+    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.6")
 }
