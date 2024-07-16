ea8d2c097d975825c272be125e674f91f3199cf2
vince <me@liuwj.me>
2024-07-16 18:06:05 +0800

    remove moditet plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 
 dependencies {
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-    api("org.moditect:moditect-gradle-plugin:1.0.0-rc3")
+    api("org.moditect:moditect:1.0.0.RC1")
     api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.6")
 }
--- buildSrc/src/main/kotlin/ktorm.modularity.gradle.kts
+++ buildSrc/src/main/kotlin/ktorm.modularity.gradle.kts
@@ -1,30 +1,37 @@
 
 plugins {
     id("kotlin")
-    id("org.moditect.gradleplugin")
 }
 
-moditect {
-    // Generate a multi-release jar, the module descriptor will be located at META-INF/versions/9/module-info.class
-    addMainModuleInfo {
-        jvmVersion.set("9")
-        overwriteExistingFiles.set(true)
-        module {
-            moduleInfoFile = file("src/main/moditect/module-info.java")
+val moditect by tasks.registering {
+    doLast {
+        // Generate a multi-release modulized jar, module descriptor position: META-INF/versions/9/module-info.class
+        val inputJar = tasks.jar.flatMap { it.archiveFile }.map { it.asFile.toPath() }.get()
+        val outputDir = file("build/moditect").apply { mkdirs() }.toPath()
+        val moduleInfo = file("src/main/moditect/module-info.java").readText()
+        val version = project.version.toString()
+        org.moditect.commands.AddModuleInfo(moduleInfo, null, version, inputJar, outputDir, "9", true).run()
+
+        // Replace the original jar with the modulized jar.
+        copy {
+            from(outputDir.resolve(inputJar.fileName))
+            into(inputJar.parent)
         }
     }
+}
 
+tasks {
+    moditect {
+        dependsOn(jar)
+    }
+    jar {
+        finalizedBy(moditect)
+    }
+}
+
+if (JavaVersion.current() >= JavaVersion.VERSION_1_9) {
     // Let kotlin compiler know the module descriptor.
-    if (JavaVersion.current() >= JavaVersion.VERSION_1_9) {
-        sourceSets.main {
-            kotlin.srcDir("src/main/moditect")
-        }
-    }
-
-    // Workaround to avoid circular task dependencies, see https://github.com/moditect/moditect-gradle-plugin/issues/14
-    afterEvaluate {
-        val compileJava = tasks.compileJava.get()
-        val addDependenciesModuleInfo = tasks.addDependenciesModuleInfo.get()
-        compileJava.setDependsOn(compileJava.dependsOn - addDependenciesModuleInfo)
+    sourceSets.main {
+        kotlin.srcDir("src/main/moditect")
     }
 }
