912d849d72e9f2af728b60217b0212ffb2bf6b26
vince <me@liuwj.me>
2024-07-22 10:40:40 +0800

    configure dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,33 @@
 group = "org.ktorm"
 version = file("ktorm.version").readLines()[0]
 
-task("printClasspath") {
-    doLast {
-        val jars = subprojects
-            .map { it.configurations["compileClasspath"] }
-            .flatMap { it.files }
-            .filterNotTo(HashSet()) { it.name.contains("ktorm") }
-            .onEach { println(it.name) }
+plugins {
+    id("org.jetbrains.dokka") version "1.9.20"
+}
 
-        val file = file("build/ktorm.classpath")
-        file.parentFile.mkdirs()
-        file.writeText(jars.joinToString(File.pathSeparator) { it.absolutePath })
-        println("Classpath written to build/ktorm.classpath")
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+tasks.dokkaHtmlMultiModule {
+    val templatesDir = System.getenv("DOKKA_TEMPLATES_DIR")
+    if (!templatesDir.isNullOrEmpty()) {
+        pluginsMapConfiguration.set(mapOf("org.jetbrains.dokka.base.DokkaBase" to """{"templatesDir": "$templatesDir"}"""))
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+
+    tasks.named<org.jetbrains.dokka.gradle.DokkaTaskPartial>("dokkaHtmlPartial") {
+        dokkaSourceSets.named("main") {
+            suppressGeneratedFiles.set(false)
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(java.net.URL("https://github.com/kotlin-orm/ktorm/blob/master/${project.name}/src/main/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
+        }
     }
 }
